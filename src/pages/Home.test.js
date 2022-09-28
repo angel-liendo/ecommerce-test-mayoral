b78c6109b893c89fe753a33a7d1860014e1d96ff
angel <aliendo@opentrends.net>
2022-09-28 16:34:55 +0200

    new click buttons plus/minus added and test card mocks fixed

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { MockedProvider } from '@apollo/react-testing';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
 import Home from '.';
 import { ALL_PRODUCTS } from '../graphql/querys';
 
@@ -18,8 +18,24 @@ const mocks = [
             id: '22',
             title: 'Polo Shirt classic',
             img: '2342444324443324443442r.PNG',
-            price: 22.99,
-            discount: 12.5,
+            price: 3,
+            discount: 8,
+            colors: [{ id: '2', name: 'blue', value: '#2062B0' }],
+          },
+          {
+            id: '11',
+            title: 'Polo Shirt classic hight price',
+            img: '2342444324443324443442r.PNG',
+            price: 222.99,
+            discount: 222.99,
+            colors: [{ id: '2', name: 'blue', value: '#2062B0' }],
+          },
+          {
+            id: '33',
+            title: 'Polo Shirt low price',
+            img: '2342444324443324443442r.PNG',
+            price: 1.99,
+            discount: 1.99,
             colors: [{ id: '2', name: 'blue', value: '#2062B0' }],
           },
         ],
@@ -40,20 +56,19 @@ describe('when the Home is mounted and mocks called', () => {
     const loader = expect(await screen.findByRole('loader')).toBeInTheDocument();
     expect(await screen.findByRole('card-list'));
     expect(await screen.findByText('Polo Shirt classic', { selector: 'span' })).toBeInTheDocument();
-    expect(
-      await screen.findByText('22,99', { selector: 'span', exact: false }),
-    ).toBeInTheDocument();
-    expect(
-      await screen.findByText('12,50', { selector: 'span', exact: false }),
-    ).toBeInTheDocument();
-    expect(await screen.findByText('Más colores', { selector: 'button' })).toBeInTheDocument();
-    expect(await screen.findByText('Añadir', { selector: 'button' })).toBeInTheDocument();
+    expect(await screen.findByText('3', { selector: 'span', exact: false })).toBeInTheDocument();
+    expect(await screen.findByText('8', { selector: 'span', exact: false })).toBeInTheDocument();
+    waitFor(() =>
+      expect(screen.findAllByText('Más colores', { selector: 'button' })).toBeInTheDocument(),
+    );
+    waitFor(() => expect(screen.findByText('Añadir', { selector: 'button' })).toBeInTheDocument());
+
     expect(loader).toBeUndefined();
   });
 });
 
-describe('when user blurs textfild and put text textfild comp', () => {
-  it('input', async () => {
+describe('when user blurs/focus textfild and put text in textfild', () => {
+  it('input search bar must filter result products', async () => {
     render(
       <MockedProvider mocks={mocks}>
         <Home />
@@ -85,5 +100,22 @@ describe('when user blurs textfild and put text textfild comp', () => {
     expect(
       await screen.findByText('No product found...', { selector: 'span' }),
     ).toBeInTheDocument();
+  });
+});
+
+describe('when user click button plus/minus', () => {
+  it('products have to be sort ascendant or descendant', async () => {
+    render(
+      <MockedProvider mocks={mocks}>
+        <Home />
+      </MockedProvider>,
+    );
+
+    expect(await screen.findByText('Polo Shirt classic', { selector: 'span' })).toBeInTheDocument();
+    const cardList = expect(await screen.findByRole('card-list'));
+
+    const button = screen.getByRole('button-ascendant');
+
+    fireEvent.click(button);
   });
 });
